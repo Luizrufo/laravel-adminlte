bac96d1f938048628668138fc75d23b59e8174a8
Luiz Fernando Rufo <1014851@senacminas.edu.br>
2024-10-09 19:41:44 -0300

    primeiro commit

---
+++ database/migrations/2024_10_09_001425_create_livro_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLivroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livro', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('autor_id')->constrained('autores')->onDelete('cascade');
+$table->foreignId('livro_id')->constrained('livros')->onDelete('cascade');
+$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livro');
+    }
+}
